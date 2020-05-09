7b63e66df80e4eb68c405a9346dace04ccaef848
Benjamin Saunders <ben.e.saunders@gmail.com>
2020-05-09 13:00:41 -0700

    Only track unordered receive state when required for unordered reads
    Ordered stream allow simpler, faster logic to determine whether a
    stream has been fully read because we are guaranteed that
    discontinuities are represented by still-buffered data. Only when
    unordered reads are in use do we need to handle the case for a gap
    between stream frames which we no longer store in any form.

--- quinn-proto/src/connection/assembler.rs
+++ quinn-proto/src/connection/assembler.rs
@@ -7,21 +7,16 @@ use crate::range_set::RangeSet;
 /// Helper to assemble unordered stream frames into an ordered stream
 #[derive(Debug, Default)]
 pub(crate) struct Assembler {
-    /// Stream offset of the first byte unread by the application. Meaningless if `unordered` is
-    /// true.
-    offset: u64,
+    state: State,
     data: BinaryHeap<Chunk>,
     defragmented: usize,
-    /// Whether any unordered reads have been performed, making this assembler unusable for ordered
-    /// reads
-    unordered: bool,
     /// Number of bytes read. Equal to offset when `unordered` is false, may or may not be
     /// otherwise.
     bytes_read: u64,
-    /// Offsets received so far
-    recvd: RangeSet,
     /// Whether to discard data
     stopped: bool,
+    /// First offset we haven't received any data at or after
+    limit: u64,
 }
 
 impl Assembler {
@@ -30,11 +25,6 @@ impl Assembler {
     }
 
     pub(crate) fn read(&mut self, buf: &mut [u8]) -> usize {
-        assert!(
-            !self.unordered,
-            "cannot perform ordered reads following unordered reads on a stream"
-        );
-
         let mut read = 0;
         loop {
             if self.consume(buf, &mut read) {
@@ -51,7 +41,15 @@ impl Assembler {
     }
 
     pub(crate) fn read_unordered(&mut self) -> Option<(u64, Bytes)> {
-        self.unordered = true;
+        if let State::Ordered { offset } = self.state {
+            // Enter unordered mode
+            let mut recvd = RangeSet::new();
+            recvd.insert(0..offset);
+            for chunk in &self.data {
+                recvd.insert(chunk.offset..chunk.offset + chunk.bytes.len() as u64);
+            }
+            self.state = State::Unordered { recvd };
+        }
         let (n, data) = self.pop()?;
         self.bytes_read += n;
         Some((n, data))
@@ -66,23 +64,28 @@ impl Assembler {
             None => return false,
         };
 
+        let offset = match self.state {
+            State::Ordered { ref mut offset } => offset,
+            _ => panic!("cannot perform ordered reads following unordered reads on a stream"),
+        };
+
         // If this chunk is either after the current offset or fully before it,
         // return directly, indicating whether the chunk can be discarded.
-        if chunk.offset > self.offset {
+        if chunk.offset > *offset {
             return false;
-        } else if (chunk.offset + chunk.bytes.len() as u64) <= self.offset {
+        } else if (chunk.offset + chunk.bytes.len() as u64) <= *offset {
             return true;
         }
 
         // Determine `start` and `len` of slice to read from chunk
-        let start = (self.offset - chunk.offset) as usize;
+        let start = (*offset - chunk.offset) as usize;
         let left = buf.len() - *read;
         let len = left.min(chunk.bytes.len() - start) as usize;
 
         // Actually write into the buffer and update the related state
         (&mut buf[*read..*read + len]).copy_from_slice(&chunk.bytes[start..start + len]);
         *read += len;
-        self.offset += len as u64;
+        *offset += len as u64;
 
         if start + len == chunk.bytes.len() {
             // This chunk has been fully consumed and can be discarded
@@ -104,7 +107,10 @@ impl Assembler {
     fn defragment(&mut self) {
         let buffered = self.data.iter().map(|c| c.bytes.len()).sum::<usize>();
         let mut buffer = BytesMut::with_capacity(buffered);
-        let mut offset = self.offset;
+        let mut offset = match self.data.peek() {
+            Some(first) => first.offset,
+            None => return,
+        };
 
         let new = BinaryHeap::with_capacity(self.data.len());
         let old = mem::replace(&mut self.data, new);
@@ -145,10 +151,14 @@ impl Assembler {
     }
 
     pub(crate) fn insert(&mut self, offset: u64, bytes: Bytes) {
-        self.recvd.insert(offset..(offset + bytes.len() as u64));
+        self.limit = self.limit.max(offset + bytes.len() as u64);
+        if let State::Unordered { ref mut recvd } = self.state {
+            recvd.insert(offset..offset + bytes.len() as u64);
+        }
         if bytes.is_empty() || self.stopped {
             return;
         }
+
         self.data.push(Chunk { offset, bytes });
         // Why 32: on the one hand, we want to defragment rarely, ideally never
         // in non-pathological scenarios. However, a pathological or malicious
@@ -169,16 +179,31 @@ impl Assembler {
 
     /// Offset after the largest byte received
     pub(crate) fn limit(&self) -> u64 {
-        self.recvd.max().map_or(0, |x| x + 1)
+        self.limit
     }
 
-    /// Whether we've received all data below `final_offset`
-    ///
-    /// `final_offset` must be greater than the offset of any received data.
-    pub(crate) fn is_complete_at(&self, final_offset: u64) -> bool {
-        debug_assert!(final_offset >= self.limit());
-        (self.recvd.len() == 1 && self.recvd.iter().next().unwrap() == (0..final_offset))
-            || final_offset == 0
+    /// Whether all data prior to `self.limit()` has been read
+    pub(crate) fn is_fully_read(&self) -> bool {
+        match self.state {
+            State::Ordered { .. } => {
+                // Any data we haven't received below `limit` is necessarily followed by
+                // still-buffered data that we *have* received which was responsible for increasing
+                // `limit`, because such data cannot be yielded to the application.
+                self.data.is_empty()
+            }
+            State::Unordered { ref recvd } => {
+                // Unordered reads allow `data` to be drained even when there are gaps below `limit`
+                // that we haven't yet received, so we need to check for such gaps
+                // explicitly. `RangeSet` is guaranteed to have a minimal representation of its
+                // ranges, and `limit` is always equal to the end of the highest range, so all data
+                // below `limit` is guaranteed to have been read by the application if `recvd` is
+                // empty, or if the buffer is empty and `recvd` contains a single range starting at
+                // zero.
+                recvd.len() <= 1
+                    && recvd.peek_min().map_or(true, |x| x.start == 0)
+                    && self.data.is_empty()
+            }
+        }
     }
 
     /// Discard all buffered data
@@ -224,6 +249,24 @@ impl PartialOrd for Chunk {
 impl PartialEq for Chunk {
     fn eq(&self, other: &Chunk) -> bool {
         (self.offset, self.bytes.len()) == (other.offset, other.bytes.len())
+    }
+}
+
+#[derive(Debug)]
+enum State {
+    Ordered {
+        offset: u64,
+    },
+    Unordered {
+        /// The set of offsets that have been received from the peer, including portions not yet
+        /// read by the application.
+        recvd: RangeSet,
+    },
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::Ordered { offset: 0 }
     }
 }
 
@@ -386,5 +429,16 @@ mod test {
         x.defragment();
         assert_eq!(x.pop(), Some((0, Bytes::from_static(b"abcdef"))));
         assert_eq!(x.pop(), Some((9, Bytes::from_static(b"jklmno"))));
+    }
+
+    #[test]
+    fn unordered_happy_path() {
+        let mut x = Assembler::new();
+        x.insert(0, Bytes::from_static(b"abc"));
+        assert_eq!(x.read_unordered(), Some((0, Bytes::from_static(b"abc"))));
+        assert_eq!(x.read_unordered(), None);
+        x.insert(3, Bytes::from_static(b"def"));
+        assert_eq!(x.read_unordered(), Some((3, Bytes::from_static(b"def"))));
+        assert_eq!(x.read_unordered(), None);
     }
 }
